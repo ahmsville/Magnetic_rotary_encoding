c4e7364e6bc2f80bb0c3b471166e1c9a125ae4c7
Ahmed Oyenuga <44074914+ahmsville@users.noreply.github.com>
2021-01-22 21:45:33 +0100

    Update MagRotaryEncoding.h

--- MagRotaryEncoding.h
+++ MagRotaryEncoding.h
@@ -10,20 +10,39 @@ This library also features a haptics controller, that allows you to connect and 
 
 #include "Arduino.h"
 
-
+#include "ADCRead.h"
 
 class MagRotaryEncoder {
 private:
 	int state, startposition = 0, prevstartposition,stepres = 100, sensor1, sensor2, sensor1_pin, sensor2_pin, prevsensor1, prevsensor2, setresolution = 1;
 	int Neutral[2], S1_Northpole[2], S1_Southpole[2], S1_North, S1_South, S2_Northpole[2], S2_Southpole[2], S2_North, S2_South, Mid;
-	int count, tempcount, bound = 20, stablerange = 10, tempanavaldiff, multiplier = 20, timetomultiply = 20;
+	int count, tempcount, bound = 10, stablerange = 10, tempanavaldiff, multiplier = 10, timetomultiply = 10;
 	int southRegion, northRegion;
 	long rotationrate = 0, timetracker = 0;
 	int haptics_pin = 0, haptics_duration = 0, haptics_strength = 0, haptics_ontime, haptics_offtime, haptics_state;
 	bool use_extended_resolution = false;
-	int countt = 0;
+	int countt = 0, distance = 0;
+	int SensorINTpin[2];
+
+	int tempstartposition = 0, tempsensor1, tempsensor2, tempprevsensor1, tempprevsensor2;
+
+	bool useInterrupt = false;
+	bool interruptFired = false;
+
+	bool INT1fired = false;
+	bool INT2fired = false;
+
+	bool INTProcessed = true;
+
+	//typedef void (MagRotaryEncoder::* ISRfunctions)(); // new data type 
+	//ISRfunctions SensorISRfunctions[2];
+
+	ADCRead adcVal;
+
 public:
+
 	MagRotaryEncoder(int s1, int s2);
+	MagRotaryEncoder(int s1, int s2, int s1INTpin, int s2INTpin);
 	void set_poleStateValues(int sensornum, int np, int nu, int sp);  //set ADC values for the poles (northpole, neutral, southpole)
 	void initialize_encoder();  //initialize encoder
 	void recaliberate_startPosition();  //sets the start position based on the ADC values
@@ -37,7 +56,19 @@ public:
 	void set_resolution(int percent);
 	void set_haptics(int pin, int duration, int strength);   //use to set haptics variables (arduino pwm pin, duration of haptics(ms), strength from 0-255)
 	void haptics(int state);
+
+	void setsleep(bool slpact);
+	bool readsleep();
+
+	void sensor1_INT();
+	void sensor2_INT();
+	
+	int get_sensorINTpin(int sensornum);  //
+	
+
+
 };
+
 
 
 #endif // !MagRotaryEncoder_h
